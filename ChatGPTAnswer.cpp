6145dec04b1044fc2343f10bbb0ee475ddc69353
Renata Espinosa <114301354+Renata1214@users.noreply.github.com>
2024-02-01 05:57:02 +0000

    ChatGPTAnswer

--- ChatGPTAnswer.cpp
+++ ChatGPTAnswer.cpp
@@ -5,12 +5,12 @@ using std:: cout;
 
 // Define a struct for the vector
 struct Vector {
-    int capacity; // Capacity of the vector
-    int size;     // Current size of the vector
+    int capacity1; // Capacity of the vector
+    int size1;     // Current size of the vector
     Payload* elements; // Array to hold elements
 
-    Vector() : capacity(1), size(0) {
-        elements = new Payload[capacity];
+    Vector() : capacity1(1), size1(0) {
+        elements = new Payload[capacity1];
     }
 
     // Destructor to deallocate memory
@@ -20,12 +20,12 @@ struct Vector {
 
     // Method to add an element to the end of the vector
     void push_back(const Payload& payload) {
-        if (size >= capacity) {
+        if (size1 >= capacity1) {
             // Double the capacity
-            capacity *= 2;
-            Payload* new_elements = new Payload[capacity];
+            capacity1 *= 2;
+            Payload* new_elements = new Payload[capacity1];
             // Copy existing elements to the new array
-            for (int i = 0; i < size; ++i) {
+            for (int i = 0; i < size1; ++i) {
                 new_elements[i] = elements[i];
             }
             // Delete the old array
@@ -33,29 +33,29 @@ struct Vector {
             // Point to the new array
             elements = new_elements;
         }
-        elements[size++] = payload;
+        elements[size1++] = payload;
     }
 
     // Method to remove the last element from the vector
     void pop_back() {
-        if (size > 0) {
-            --size;
+        if (size1 > 0) {
+            --size1;
         }
     }
 
     // Method to return the current size of the vector
-    int get_size() const {
-        return size;
+    int size() const {
+        return size1;
     }
 
     // Method to return the current capacity of the vector
-    int get_capacity() const {
-        return capacity;
+    int capacity() const {
+        return capacity1;
     }
 
     // Method to print the elements of the vector
     void print() const {
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < size1; ++i) {
             elements[i].print(); // Assuming Payload has a print() method
             cout << " ";
         }
