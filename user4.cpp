31c184cced66bf54aef600774211db1962e5a480
Renata Espinosa <114301354+Renata1214@users.noreply.github.com>
2024-02-01 10:25:03 +0400

    Update user4.cpp

--- user4.cpp
+++ user4.cpp
@@ -9,12 +9,15 @@ int main()
  {
  cout << "Vector size/capacity: " << v.size() << '/' <<
 v.capacity() << '\n';
+ v.print();
+ cout << '\n';
  };
+
+
  {
  Vector v;
  v.push_back("apple");
 showVector(v);
-// cout << v.tryout().tryout1()<< '\n';
 v.push_back("pear");
 v.push_back("banana");
 showVector(v);
@@ -23,11 +26,11 @@ showVector(v);
  }
 }
 
-
-
 /*
-Output:
 Vector size/capacity: 1/1
+(apple)
 Vector size/capacity: 3/4
+(apple)(pear)(banana)
 Vector size/capacity: 2/4
+(apple)(pear)
 */
